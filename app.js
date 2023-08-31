515f74cb5e9a743a98d4faa6dacb46071f5ea9b5
steventran1983 <baothangvn@gmail.com>
2023-08-31 14:52:05 -0500

    a

--- app.js
+++ app.js
@@ -4,19 +4,18 @@ import morgan from "morgan";
 import * as dotenv from "dotenv";
 dotenv.config();
 import indexRoute from "./routes/indexRoute.js";
-
+const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 
-const PORT = process.env.PORT || 4000;
-
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
-})
+});
 
 app.use("/api", indexRoute);
 
-app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
