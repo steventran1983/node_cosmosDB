92d32b143f9e3481b7a3d4cebb4a46a425519918
steventran1983 <baothangvn@gmail.com>
2023-08-31 21:01:09 -0500

    change

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import * as dotenv from "dotenv";
-dotenv.config();
+// import * as dotenv from "dotenv";
+// dotenv.config();
 import indexRoute from "./routes/indexRoute.js";
-const PORT = process.env.PORT || 3000;
 const app = express();
+const PORT = process.env.PORT || 3000;
 app.use(cors());
-app.use(express.json());
-app.use(morgan("tiny"));
+// app.use(express.json());
+// app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! This is the app");
 });
 
-app.use("/api", indexRoute);
+// app.use("/api", indexRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
