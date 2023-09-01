44e8a4c718a249c0edc33105a5ae04aa057951f5
steventran1983 <baothangvn@gmail.com>
2023-08-31 22:12:35 -0500

    Thang

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const dotenv = require("dotenv");
-dotenv.config();
-
-const indexRoute = require("./routes/indexRoute.js");
-
+// const cors = require("cors");
+// const morgan = require("morgan");
+// const dotenv = require("dotenv");
+// dotenv.config();
+// const indexRoute = require("./routes/indexRoute.js");
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors());
-app.use(express.json());
-app.use(morgan("tiny"));
+// app.use(cors());
+// app.use(express.json());
+// app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Hello World! This is the app");
 });
 
-app.use("/api", indexRoute);
+// app.use("/api", indexRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
