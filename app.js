924dcf3730e501fb6fcbab1d70d5124cd682bcfc
steventran1983 <baothangvn@gmail.com>
2023-08-31 21:26:13 -0500

    change

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import express from "express";
-import cors from "cors";
+const express = require("express");
+const cors = require("cors");
 // import * as dotenv from "dotenv";
 // dotenv.config();
-import indexRoute from "./routes/indexRoute.js";
+// const indexRoute = require("./routes/index.js");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
   res.send("Hello World! This is the app ddii");
 });
 
-app.use("/api", indexRoute);
+// app.use("/api", indexRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- models/cosmosDB.js
+++ models/cosmosDB.js
@@ -1,5 +1,6 @@
 import { CosmosClient } from "@azure/cosmos";
-import * as dotenv from "dotenv";
+const { CosmosClient } = require("@azure/cosmos");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const endpoint = process.env.COSMOSDB_ENDPOINT;
