f7d53debd611a076792f5d91376bf9407fa65130
steventran1983 <baothangvn@gmail.com>
2023-08-31 23:16:07 -0500

    get all employees

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -5,18 +5,16 @@ const client = require("../models/cosmosDB");
 const getEmployees = async (req, res) => {
   const databaseId = process.env.COSMOSDB_DATABASE;
   const containerId = process.env.COSMOSDB_CONTAINER_EMPLOYEES;
-  console.log(databaseId, containerId);
-  res.json({ message: databaseId, containerId: containerId });
-  // try {
-  //   const { resources: items } = await client
-  //     .database(databaseId)
-  //     .container(containerId)
-  //     .items.query("SELECT * FROM d")
-  //     .fetchAll();
-  //   res.status(200).json(items);
-  // } catch (error) {
-  //   res.status(500).json({ message: error.message });
-  // }
+  try {
+    const { resources: items } = await client
+      .database(databaseId)
+      .container(containerId)
+      .items.query("SELECT * FROM d")
+      .fetchAll();
+    res.status(200).json(items);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = getEmployees;
