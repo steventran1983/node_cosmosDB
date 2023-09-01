4262120e62ff09eef3fcd2e87b8191be83a4e972
steventran1983 <baothangvn@gmail.com>
2023-08-31 22:16:22 -0500

    c

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ dotenv.config();
 // const indexRoute = require("./routes/indexRoute.js");
 const app = express();
 const PORT = process.env.PORT || 3000;
-// app.use(cors());
-// app.use(express.json());
-// app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Hello World! This is the app");
