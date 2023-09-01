31439eee7bb2bfb2ebeb86791e67bb70ca60dc09
steventran1983 <baothangvn@gmail.com>
2023-08-31 22:48:10 -0500

    b

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { getEmployees } = require("../controllers/employeeController");
 const router = express.Router();
+const getEmployees = require("../controllers/employeeController");
 
 router.get("/", (req, res, next) => {
   res.send("This is employee Route!");
