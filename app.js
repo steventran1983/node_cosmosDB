c779447414ac4abb2d5efc16a6ff6d5d5b67fef0
steventran1983 <baothangvn@gmail.com>
2023-08-31 21:06:27 -0500

    change 2

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
+// import express from "express";
+// import cors from "cors";
+// import morgan from "morgan";
+// // import * as dotenv from "dotenv";
+// // dotenv.config();
+// import indexRoute from "./routes/indexRoute.js";
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+// app.use(cors());
+// // app.use(express.json());
+// // app.use(morgan("tiny"));
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World! This is the app");
+// });
+
+// // app.use("/api", indexRoute);
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-// import * as dotenv from "dotenv";
-// dotenv.config();
-import indexRoute from "./routes/indexRoute.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors());
-// app.use(express.json());
-// app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World! This is the app");
+  {
+    res.send("Hello World! This is the app");
+  }
 });
-
-// app.use("/api", indexRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
