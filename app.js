06202a1113bf09f2e6b13268507f3fdb12243ed1
steventran1983 <baothangvn@gmail.com>
2023-08-31 22:15:30 -0500

    b

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
-// const cors = require("cors");
-// const morgan = require("morgan");
-// const dotenv = require("dotenv");
-// dotenv.config();
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
 // const indexRoute = require("./routes/indexRoute.js");
 const app = express();
 const PORT = process.env.PORT || 3000;
