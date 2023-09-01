b4de3cd2ef9dbccd19fbcc23eb54e3b5256d3be4
steventran1983 <baothangvn@gmail.com>
2023-08-31 22:56:34 -0500

    thang

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const getEmployees = require("../controllers/employeeController");
+// const getEmployees = require("../controllers/employeeController");
 
 router.get("/", (req, res, next) => {
   res.send("This is employee Route!");
 });
 
-router.use("/all", getEmployees);
+// router.use("/all", getEmployees);
 
 module.exports = router;
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const employeeRoute = require("./employeeRoute");
+const employeeRoute = require("./employeeRoute");
 
 // const productRoute = require("./productRoute.js");
 const router = express.Router();
@@ -8,7 +8,7 @@ router.get("/", (req, res) => {
   res.send("This is API Route!");
 });
 
-// router.use("/employees", employeeRoute);
+router.use("/employees", employeeRoute);
 // router.use("/product", productRoute);
 
 module.exports = router;
