5e7ba90fdf2c0f51b3df9b06f8a7379bf91637df
steventran1983 <baothangvn@gmail.com>
2023-08-31 22:59:51 -0500

    Thang

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -3,18 +3,19 @@ dotenv.config();
 const client = require("../models/cosmosDB");
 
 const getEmployees = async (req, res) => {
-  const databaseId = process.env.COSMOSDB_DATABASE;
-  const containerId = process.env.COSMOSDB_CONTAINER_EMPLOYEES;
-  try {
-    const { resources: items } = await client
-      .database(databaseId)
-      .container(containerId)
-      .items.query("SELECT * FROM d")
-      .fetchAll();
-    res.status(200).json(items);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+  res.json({ message: "This is get all employee" });
+  // const databaseId = process.env.COSMOSDB_DATABASE;
+  // const containerId = process.env.COSMOSDB_CONTAINER_EMPLOYEES;
+  // try {
+  //   const { resources: items } = await client
+  //     .database(databaseId)
+  //     .container(containerId)
+  //     .items.query("SELECT * FROM d")
+  //     .fetchAll();
+  //   res.status(200).json(items);
+  // } catch (error) {
+  //   res.status(500).json({ message: error.message });
+  // }
 };
 
 module.exports = getEmployees;
