f4f8392fccc0e3736c5dd32e66914b98ca702225
steventran1983 <baothangvn@gmail.com>
2023-08-31 21:19:09 -0500

    ab

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 import express from "express";
 import cors from "cors";
-import morgan from "morgan";
-import * as dotenv from "dotenv";
-dotenv.config();
+// import * as dotenv from "dotenv";
+// dotenv.config();
 import indexRoute from "./routes/indexRoute.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
-app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Hello World! This is the app ddii");
