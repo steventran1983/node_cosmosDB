d38a0c090380890c02d8f773021eb532c356f640
steventran1983 <baothangvn@gmail.com>
2023-08-31 22:17:22 -0500

    adding route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 dotenv.config();
-// const indexRoute = require("./routes/indexRoute.js");
+const indexRoute = require("./routes/indexRoute.js");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
@@ -11,10 +11,10 @@ app.use(express.json());
 app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World! This is the app");
+  res.send("Hello World! This is the app te le");
 });
 
-// app.use("/api", indexRoute);
+app.use("/api", indexRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
